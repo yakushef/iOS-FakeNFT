6fc81a558fe3d9f60e30752844bafce64e252fc0
Aleksei Iakushev <av.yakush@gmail.com>
2023-10-10 21:17:28 +0300

    Добавлены шрифты из Фигмы и статические переменные для них

---
+++ FakeNFT/DesignSystem/Fonts/Fonts.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension UIFont {
+    // Bold Fonts
+    
+    static var bold17 = UIFont(name: "SF-Pro-Text-Bold", size: 17)
+    static var bold22 = UIFont(name: "SF-Pro-Text-Bold", size: 22)
+    static var bold32 = UIFont(name: "SF-Pro-Text-Bold", size: 32)
+    static var bold34 = UIFont(name: "SF-Pro-Text-Bold", size: 34)
+    
+    // Regular fonts
+    
+    static var regular13 = UIFont(name: "SF-Pro-Text-Regular", size: 13)
+    static var regular15 = UIFont(name: "SF-Pro-Text-Regular", size: 15)
+    static var regular17 = UIFont(name: "SF-Pro-Text-Regular", size: 17)
+    
+    // Medium Fonts
+    
+    static var medium10 = UIFont(name: "SF-Pro-Text-Medium", size: 10)
+}
