4e1438df27c415ad32754d2e8ad528a7e11da0b1
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-10-11 16:32:12 +0200

    Linked ProfileViewController with storyboard view controller

--- FakeNFT/ProfileModule/ProfileViewController.swift
+++ FakeNFT/ProfileModule/ProfileViewController.swift
@@ -5,4 +5,12 @@
 //  Created by Антон Кашников on 11/10/2023.
 //
 
-import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
