60e573ba3a064ceb0c196ecbfbb9ed6bc9249ea1
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-10-11 16:39:48 +0200

    Changed background color of profile view controller

--- FakeNFT/ProfileModule/ProfileViewController.swift
+++ FakeNFT/ProfileModule/ProfileViewController.swift
@@ -11,6 +11,6 @@ final class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        view.backgroundColor = .ypWhite
     }
 }
