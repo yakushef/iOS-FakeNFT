ae080ede733e5cc3b0349fdafe40724dae8a97a0
Aleksei Iakushev <av.yakush@gmail.com>
2023-10-17 23:11:22 +0300

    Сверстана экран оплаты

--- FakeNFT/CartFlow/CheckoutView/CheckoutViewController.swift
+++ FakeNFT/CartFlow/CheckoutView/CheckoutViewController.swift
@@ -7,12 +7,76 @@
 
 import UIKit
 
-class CheckoutViewController: UIViewController {
+final class CheckoutViewController: UIViewController {
+    private lazy var currencyCollection: UICollectionView = {
+       let currencyCollection = UICollectionView(frame: CGRect(),
+                                                 collectionViewLayout: UICollectionViewLayout())
+       return currencyCollection
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .ypWhite
         navigationItem.title = "Выберете способ оплаты"
+        
+        setupUI()
     }
+    
+    func setupUI() {
+        setupCollection()
+        
+        let paymentView = MakePaymentView()
+        view.addSubview(paymentView)
+        paymentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            paymentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            paymentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            paymentView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+    
+    private func setupCollection() {
+        let frame = view.safeAreaLayoutGuide.layoutFrame
+        let layout = UICollectionViewFlowLayout()
+        currencyCollection = UICollectionView(frame: frame, collectionViewLayout: layout)
+        
+        currencyCollection.dataSource = self
+        currencyCollection.delegate = self
+        currencyCollection.contentInset = UIEdgeInsets(top: 20,
+                                                       left: 16,
+                                                       bottom: 20,
+                                                       right: 16)
+        currencyCollection.register(CurrencyCell.self)
+        view.addSubview(currencyCollection)
+    }
+}
 
+extension CheckoutViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        7
+    }
+}
+
+extension CheckoutViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = (collectionView.frame.width - 7) / 2  - collectionView.contentInset.left
+        return CGSize(width: cellWidth, height: 46)
+    }
+}
+
+extension CheckoutViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: CurrencyCell = collectionView.dequeueReusableCell(indexPath: indexPath)
+        cell.configureCell(for: nil)
+        return cell
+    }
 }
---
+++ FakeNFT/CartFlow/CheckoutView/MakePaymentView.swift
@@ -0,0 +1,92 @@
+//
+//  MakePaymentView.swift
+//  FakeNFT
+//
+//  Created by Aleksey Yakushev on 17.10.2023.
+//
+
+import UIKit
+
+class MakePaymentView: UIView {
+    private let insets = UIEdgeInsets(top: 16,
+                                      left: 16,
+                                      bottom: 16,
+                                      right: 16)
+    private lazy var payButton: UIButton = {
+        let payButton = UIButton(type: .system)
+        payButton.setTitle("Оплатить", for: .normal)
+        payButton.titleLabel?.font = .bold17
+        payButton.backgroundColor = .ypBlack
+        payButton.tintColor = .ypWhite
+        payButton.clipsToBounds = true
+        payButton.layer.cornerRadius = 16
+        return payButton
+    }()
+    
+    private lazy var userAgreementButton: UIButton = {
+        let agreementButton = UIButton(type: .system)
+        agreementButton.tintColor = .blueUniversal
+        agreementButton.setTitle("Пользовательского соглашения", for: .normal)
+        agreementButton.titleLabel?.font = .regular13
+        agreementButton.backgroundColor = .clear
+        agreementButton.contentHorizontalAlignment = .left
+        return agreementButton
+    }()
+    
+    private lazy var disclaimerLabel: UILabel = {
+        let disclaimerLabel = UILabel()
+        disclaimerLabel.text = "Совершая покупку, вы соглашаетесь с условиями"
+        disclaimerLabel.textColor = .ypBlack
+        disclaimerLabel.font = .regular13
+        disclaimerLabel.backgroundColor = .clear
+        disclaimerLabel.textAlignment = .left
+        return disclaimerLabel
+    }()
+
+    //MARK: - Lifecycle
+    
+    init() {
+        super.init(frame: .zero)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - UI setup
+    
+    func setupUI() {
+        backgroundColor = .ypLightGrey
+        clipsToBounds = true
+        layer.cornerRadius = 12
+        layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        addSubview(payButton)
+        payButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            payButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
+            payButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
+            payButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -insets.bottom),
+            payButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
+        
+        addSubview(userAgreementButton)
+        userAgreementButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userAgreementButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
+            userAgreementButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
+            userAgreementButton.bottomAnchor.constraint(equalTo: payButton.topAnchor, constant: -insets.top)
+        ])
+        
+        addSubview(disclaimerLabel)
+        disclaimerLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            disclaimerLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
+            disclaimerLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
+            disclaimerLabel.bottomAnchor.constraint(equalTo: userAgreementButton.topAnchor),
+            disclaimerLabel.topAnchor.constraint(equalTo: topAnchor, constant: insets.top)
+        ])
+    }
+
+}
