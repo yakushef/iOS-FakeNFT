7cc78abab18053583a1ca9ec966981196cf1c022
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-10-17 14:23:52 +0200

    Created enums for fonts

--- FakeNFT/DesignSystem/Fonts/Fonts.swift
+++ FakeNFT/DesignSystem/Fonts/Fonts.swift
@@ -2,16 +2,22 @@ import UIKit
 
 extension UIFont {
     // Bold Fonts
-    static let bold17 = UIFont(name: "SF Pro Text Bold", size: 17)
-    static let bold22 = UIFont(name: "SF Pro Text Bold", size: 22)
-    static let bold32 = UIFont(name: "SF Pro Text Bold", size: 32)
-    static let bold34 = UIFont(name: "SF Pro Text Bold", size: 34)
+    enum Bold {
+        static let small = UIFont(name: "SF Pro Text Bold", size: 17)
+        static let medium = UIFont(name: "SF Pro Text Bold", size: 22)
+        static let large = UIFont(name: "SF Pro Text Bold", size: 32)
+        static let xlarge = UIFont(name: "SF Pro Text Bold", size: 34)
+    }
     
     // Regular fonts
-    static let regular13 = UIFont(name: "SF Pro Text Regular", size: 13)
-    static let regular15 = UIFont(name: "SF Pro Text Regular", size: 15)
-    static let regular17 = UIFont(name: "SF Pro Text Regular", size: 17)
+    enum Regular {
+        static let small = UIFont(name: "SF Pro Text Regular", size: 13)
+        static let medium = UIFont(name: "SF Pro Text Regular", size: 15)
+        static let large = UIFont(name: "SF Pro Text Regular", size: 17)
+    }
     
     // Medium Fonts
-    static let medium10 = UIFont(name: "SF Pro Text Medium", size: 10)
+    enum Medium {
+        static let medium = UIFont(name: "SF Pro Text Medium", size: 10)
+    }
 }
