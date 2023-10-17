50caa98f3a9aa8848a6093e8b7238cbce3b85b22
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-10-17 14:19:54 +0200

    Changed font static var to let

--- FakeNFT/DesignSystem/Fonts/Fonts.swift
+++ FakeNFT/DesignSystem/Fonts/Fonts.swift
@@ -2,19 +2,16 @@ import UIKit
 
 extension UIFont {
     // Bold Fonts
-    
-    static var bold17 = UIFont(name: "SF Pro Text Bold", size: 17)
-    static var bold22 = UIFont(name: "SF Pro Text Bold", size: 22)
-    static var bold32 = UIFont(name: "SF Pro Text Bold", size: 32)
-    static var bold34 = UIFont(name: "SF Pro Text Bold", size: 34)
+    static let bold17 = UIFont(name: "SF Pro Text Bold", size: 17)
+    static let bold22 = UIFont(name: "SF Pro Text Bold", size: 22)
+    static let bold32 = UIFont(name: "SF Pro Text Bold", size: 32)
+    static let bold34 = UIFont(name: "SF Pro Text Bold", size: 34)
     
     // Regular fonts
-    
-    static var regular13 = UIFont(name: "SF Pro Text Regular", size: 13)
-    static var regular15 = UIFont(name: "SF Pro Text Regular", size: 15)
-    static var regular17 = UIFont(name: "SF Pro Text Regular", size: 17)
+    static let regular13 = UIFont(name: "SF Pro Text Regular", size: 13)
+    static let regular15 = UIFont(name: "SF Pro Text Regular", size: 15)
+    static let regular17 = UIFont(name: "SF Pro Text Regular", size: 17)
     
     // Medium Fonts
-    
-    static var medium10 = UIFont(name: "SF Pro Text Medium", size: 10)
+    static let medium10 = UIFont(name: "SF Pro Text Medium", size: 10)
 }
